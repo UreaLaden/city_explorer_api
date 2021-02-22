4aaae09da50b440368dbf5fd2fd7a4ee5f8f34d8
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-02-22 18:45:13 -0500

    Added linter and gitignore

--- server.js
+++ server.js
@@ -25,6 +25,8 @@
     To see them in javascript: 'process.env.VARNAME'
 
     As devs, we can save our environment variables in a file called '.env'
+    
+    When data is sent from the client to the backend, it comes in a property: ' request.query'
     */
 //============================Packages================================
 
@@ -55,7 +57,44 @@ app.get('/puppy',puppyCallback); // this is a route that lives at /puppy and sen
 // the secon parameter will always be given response data
 function puppyCallback(req,res){
     ginger.friends++;
-    res.send(ginver);
+    res.send(ginger);
+}
+
+app.get('/location',handleGetLocation(res,req));
+   
+    const dataFromTheFile = require('./data/file.json'); //in an express server, we can synchronously get data from a local json file without a .then
+
+    /*res.send({
+            "search_query": "seattle",
+            "formatted_query": "Seattle, WA, USA",
+            "latitude": "47.606210",
+            "longitude": "-122.332071"
+          });        
+          */
+/*
+Lab requirement approximation: Create a route name '/location' that sends location data to the client. An example of how the data should look liks is this
+{
+  "search_query": "seattle",
+  "formatted_query": "Seattle, WA, USA",
+  "latitude": "47.606210",
+  "longitude": "-122.332071"
+}
+*/
+    //const output = new Location(dataFromTheFile, req.query);
+    //console.log(req.query);
+    const output = {
+        search_query:'',
+        formatted_query:dataFromTheFile[0].display_name,
+        latitude:dataFromTheFile[0].lat,
+        longitude: dataFromTheFile[0].lon
+}
+res.send(output);
+
+function Location(dataFromTheFile,cityName){
+    this.search_query = cityName;
+    this.formatted_query = dataFromTheFile[0].display_name;
+    this.latitude = dataFromTheFile[0].lat;
+    this.longitude = dataFromTheFile[0].lon;
 }
 
 //============================Initialization================================
